2235942bc6fe1e23b4f97a8a5d42443854d12011
AurelianoSilva <aurelianos18@gmail.com>
2022-10-21 21:13:24 -0300

    inclusão de referencias no projeto

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
